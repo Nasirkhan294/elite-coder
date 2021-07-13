d8a2d77edf7e261e2a0d6b1bd997e177c3bfa20e
Nasirkhan294 <nasirbhatti.nk@gmail.com>
2021-07-13 14:35:34 +0500

    Update content in portfolio and about sections

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ window.addEventListener("load", () => {
     document.querySelector(".page-loader").classList.add("fade-out");
     setTimeout(() =>{
         document.querySelector(".page-loader").style.display = "none";
-    },800);
+    },500);
 });
 
 /*--------------------- Home Toggle Navbar ----------------*/
